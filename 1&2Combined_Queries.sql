1d912676e94559365321d457233bc9972a8e3e89
Aaron-Pin <114532927+Aaron-Pin@users.noreply.github.com>
2022-11-02 20:52:30 -0700

    Update 1&2Combined_Queries.sql

--- 1&2Combined_Queries.sql
+++ 1&2Combined_Queries.sql
@@ -1,6 +1,7 @@
 -- First I combine my streaminghistory0 and streaminghistory1 table together using UNION ALL. Then, for all my duplicate tracks, I add their msPlayed by their trackName.
 -- This query ultimately eliminates duplicates and finds my total playtime for specific tracks. This must be done as the spotify data adds a 'trackName' each time you 
 -- stream it, making your data have many duplicates. The queries below are performed on seperate tabs.
+-- Note, 'spotifyhistory' is my dataset ID.
 
 WITH dupes AS (
   SELECT 
